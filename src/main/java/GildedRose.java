5f4d87b94637f48b0726634bea10497432f75d99
Hunain Parekh <hunainparekh85@gmail.com>
2023-01-15 00:41:41 +0500

    code refactoring

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -1,5 +1,3 @@
-import org.apache.commons.lang3.StringUtils;
-
 public class GildedRose {
 
     public String name;
@@ -55,50 +53,6 @@ public class GildedRose {
             case "Backstage passes to a TAFKAL80ETC concert":
                 backstage_tick();
                 return;
-        }
-
-        if(!StringUtils.equals(name, "Aged Brie") && !StringUtils.equals(name, "Backstage passes to a TAFKAL80ETC concert")) {
-            if(quality > 0) {
-                if(!StringUtils.equals(name, "Sulfuras, Hand of Ragnaros")) {
-                    quality -= 1;
-                }
-            }
-        } else {
-            if(quality < 50) {
-                quality += 1;
-                if(StringUtils.equals(name, "Backstage passes to a TAFKAL80ETC concert")) {
-                    if(daysRemaining < 11) {
-                        if(quality < 50) {
-                            quality += 1;
-                        }
-                    }
-                    if(daysRemaining < 6) {
-                        if(quality < 50) {
-                            quality += 1;
-                        }
-                    }
-                }
-            }
-        }
-        if(!StringUtils.equals(name, "Sulfuras, Hand of Ragnaros")) {
-            daysRemaining -= 1;
-        }
-        if(daysRemaining < 0) {
-            if(!StringUtils.equals(name, "Aged Brie")) {
-                if(!StringUtils.equals(name, "Backstage passes to a TAFKAL80ETC concert")) {
-                    if(quality > 0) {
-                        if(!StringUtils.equals(name, "Sulfuras, Hand of Ragnaros")) {
-                            quality -=1;
-                        }
-                    }
-                } else {
-                    quality = quality - quality;
-                }
-            } else {
-                if(quality < 50) {
-                    quality += 1;
-                }
-            }
         }
     }
 }
