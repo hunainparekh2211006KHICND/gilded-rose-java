91361ad789e99d8c5fbc68fb0ba0b1c8233972f8
Hunain Parekh <hunainparekh85@gmail.com>
2023-01-15 00:36:35 +0500

    3rd , fourth and fifth backstage test passed

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -21,13 +21,9 @@ public class GildedRose {
 
     public void brie_tick(){
         daysRemaining -=1;
-        if(quality ==50){
-            return;
-        }
+        if(quality ==50) return;
         quality+=1;
-        if(quality ==50){
-            return;
-        }
+        if(quality ==50) return;
         if(daysRemaining <= 0) quality +=1;
     }
 
@@ -37,6 +33,7 @@ public class GildedRose {
 
     public void backstage_tick(){
         daysRemaining -=1;
+        if(quality == 50) return;
         quality +=1;
         if(daysRemaining <=10) quality +=1;
     }
